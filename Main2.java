9589ee21c594df971b5e953e48c19038db7367cf
3thanG8 <144505142+3thanG8@users.noreply.github.com>
2023-09-11 21:37:03 -0400

    Add files via upload

---
+++ Main2.java
@@ -0,0 +1,19 @@
+public class Main2
+{
+  public static void main(String[] args)
+  {
+    int Health;
+    Health = 20;
+    System.out.println("HP:" + Health);
+    
+    double Damage = 3.5;
+    System.out.println("DMG:" + Damage);
+    
+    boolean onFire = false;
+    System.out.println(onFire);
+    
+    final boolean Hazards = true;
+    System.out.println(Hazards);
+    
+  }
+}

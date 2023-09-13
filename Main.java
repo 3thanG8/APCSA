f374b08bd4c9a1df2399e95a6a7292c19219217d
3thanG8 <144505142+3thanG8@users.noreply.github.com>
2023-09-13 10:21:18 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -16,3 +16,29 @@ public class Main
     System.out.println("Hazards:" + Hazards);    
   }
 }
+
+
+public class Main
+{
+  public static void main(String[] args)
+  {
+    System.out.println("Hello, world");
+    System.out.println("I am Ethan");
+  
+    int Health;
+    Health = 20;
+    System.out.println("HP:" + Health);
+    
+    double Damage = 2.5;
+    System.out.println("DMG:" + Damage);
+    
+    final boolean Hazards = true;
+    System.out.println("Hazards:" + Hazards);
+    
+    double buff = Damage + 0.25;
+    System.out.println("Buffed DMG:" + buff);
+    
+    double debuff = 0.35;
+    System.out.println("Debuffed DMG:" + (Damage - debuff = debuffed);
+  }
+}

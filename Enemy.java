fc0fb62ffca0f58083498b525f63462cfb31a195
3thanG8 <144505142+3thanG8@users.noreply.github.com>
2023-09-24 23:47:54 -0400

    Add files via upload

---
+++ Enemy.java
@@ -0,0 +1,29 @@
+public class Enemy {
+  private double hp = 25.0;
+  private int dmg = 2;
+  
+  public int atk() { 
+    System.out.println("Enemy attacks!");
+    return dmg;
+  }
+  
+  public void block(int ovrdmg) {
+    hp -= ovrdmg;
+    System.out.println("HP: " + hp);
+  }
+  
+  public double hpLeft() {
+    return hp;
+  }
+  
+  public int dmgPOW() {
+    return dmg;
+  }
+}
+  
+  
+    
+    
+  
+
+    

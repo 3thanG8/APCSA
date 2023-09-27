b6771f4d187504180cb78aa1c1f84473a64fb80d
3thanG8 <144505142+3thanG8@users.noreply.github.com>
2023-09-26 22:57:03 -0400

    Add files via upload

---
+++ Keyboard.java
@@ -0,0 +1,46 @@
+public class Keyboard {
+
+  private String material;
+  private String keycolor;
+  private double price;
+  public Keyboard(String m, String k, double p)
+  {
+    material = m;
+    keycolor = k;
+    price = p;
+  }
+    
+  public Keyboard (String m, String k) 
+  {
+    material = m;
+    keycolor = k;
+  }
+  
+  public String getString() {
+    return (material + keycolor);
+  }
+  
+    public String getMat() {
+      return material;
+    }
+    
+    public String getKey() {
+      return keycolor;
+    }
+    
+    public double getPrice() {
+      return price;
+    }
+    public void getKeyboard() {
+      System.out.println(getMat());
+      System.out.println(getKey());
+      System.out.println(getPrice());
+    }
+}
+
+    
+
+
+
+
+    

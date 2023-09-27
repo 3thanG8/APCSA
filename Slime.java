1b06ed52144b063bd26f0934e3a49800f5c8c230
3thanG8 <144505142+3thanG8@users.noreply.github.com>
2023-09-27 10:19:03 -0400

    Add files via upload

---
+++ Slime.java
@@ -0,0 +1,21 @@
+public class Slime {
+  private double hp = 20.0;
+  private double atk = 1.5;
+  private double acid = 2.0;
+  
+  public double getHP() {
+    return hp;
+  }
+  
+  public double getATK() {
+    return atk;
+  }
+  
+  public double getAcidATK() {
+    return acid;
+  }
+  
+  public double getStats() {
+  }
+}
+    

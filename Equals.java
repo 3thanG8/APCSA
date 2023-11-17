d7a8a47ec07673d986c4622b3e37ce1d5f011776
3thanG8 <144505142+3thanG8@users.noreply.github.com>
2023-11-17 09:51:54 -0500

    Create Equals.java

---
+++ Equals.java
@@ -0,0 +1,10 @@
+// 11/17/23
+public class Equals {
+  public boolean equals(int a, int b) {
+    if (a <= b) return true;
+    else if (a == b) return false;
+    else if (a >= b) return true;
+    else return false;
+  }
+}
+    
